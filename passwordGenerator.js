b6a70d7146eb99d9361c40f46db27f43dbddc9a3
kuduzovkhamzat@gmail.com <khamzat995@gmail.com>
2021-05-19 17:34:19 +0300

    Релиз 4

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,40 +1,44 @@
-function passwordGenerator(num, string) {
-  const password = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let newPasswordSplit = password.split('', num).sort(() => Math.random() - 0.5).join('');
-
+function passwordGenerator(num, string, count = 1) {
   if (num < 5 || typeof num != 'number') {
-    return 'Ошибка, попробуйте снова';
+    return 'Ошибка, пожалуйста попробуйте снова';
+  }
+  if (typeof count !== 'number' || count < 1) {
+    return 'Ошибка третьего уровня';
   }
 
+  const password = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const number1 = '0123456789';
-  const symbol1 = '!#$%&()*+,-./:;<=>?@[\\]^_{|}~';
+  const symbol1 = '!#$%&()*+,-./:;<=>?@[]^_{|}~';
 
-  if (string === 'easy') {
-    let easyPassword = password
-    easyPassword = easyPassword.split('')
-    shuffle(easyPassword)
-    easyPassword = easyPassword.join('');
-    console.log(easyPassword.substr(0, num))
-  }
-  if (string === 'medium') {
-    let mediumPassword = password + number1
-    mediumPassword = mediumPassword.split('')
-    shuffle(mediumPassword)
-    mediumPassword = mediumPassword.join('');
-    console.log(mediumPassword.substr(0, num))
-  }
-  if (string === 'hard') {
-    let hardPassword = password + number1 + symbol1
-    hardPassword = hardPassword.split('')
-    shuffle(hardPassword)
-    hardPassword = hardPassword.join('');
-    console.log(hardPassword.substr(0, num))
-  }
-}
+  let passwordsArray = [];
 
-passwordGenerator(10, 'hard')
+  for (let i = 0; i < count; i++) {
+    if (string === 'easy') {
+      let easyPassword = password
+      easyPassword = easyPassword.split('')
+      shuffle(easyPassword)
+      easyPassword = easyPassword.join('');
+      passwordsArray.push(easyPassword.substr(0, num))
+    }
+    if (string === 'medium') {
+      let mediumPassword = password + number1
+      mediumPassword = mediumPassword.split('')
+      shuffle(mediumPassword)
+      mediumPassword = mediumPassword.join('');
+      passwordsArray.push(mediumPassword.substr(0, num))
+    }
+    if (string === 'hard') {
+      let hardPassword = password + number1 + symbol1
+      hardPassword = hardPassword.split('')
+      shuffle(hardPassword)
+      hardPassword = hardPassword.join('');
+      passwordsArray.push(hardPassword.substr(0, num))
+    }
+  }
+  console.log(passwordsArray);
+  }
 
 function shuffle(array) {
-  array.sort(() => Math.random() - 0.5);
+  array.sort(() => Math.random() - 0.5)
 }
 
