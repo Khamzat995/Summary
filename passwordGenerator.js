2541fdbd15910c44ffbcc314c7d56860322cf230
kuduzovkhamzat@gmail.com <khamzat995@gmail.com>
2021-05-19 11:53:10 +0300

    Релиз 2

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,6 +1,10 @@
-function passwordGenerator() {
+function passwordGenerator(num) {
   const password = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let newPasswordSplit = password.split('').sort(() => Math.random() - 0.5).join('');
   console.log(newPasswordSplit)
+
+  if (num < 5 || typeof num != 'number') {
+    console.log('Недопустимый пароль');
+  }
 }
-passwordGenerator()
+passwordGenerator(5)
